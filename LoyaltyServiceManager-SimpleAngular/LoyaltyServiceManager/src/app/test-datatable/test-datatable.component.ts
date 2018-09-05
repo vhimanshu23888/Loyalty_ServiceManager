305d790358ea039e50fca62f87021f06f9141c52
vhimanshu23888 <40313537+vhimanshu23888@users.noreply.github.com>
2018-09-06 01:15:29 +0530

    Fixed Paging, Searching and Sorting on ManageServices Page. Also, Implemented Images for Web/Windows services.

--- LoyaltyServiceManager-SimpleAngular/LoyaltyServiceManager/src/app/test-datatable/test-datatable.component.ts
+++ LoyaltyServiceManager-SimpleAngular/LoyaltyServiceManager/src/app/test-datatable/test-datatable.component.ts
@@ -19,16 +19,17 @@ const DECIMAL_FORMAT: (v: any) => any = (v: number) => v.toFixed(2);
 })
 export class TestDatatableComponent implements OnInit {
   columns: ITdDataTableColumn[] = [
-    { name: 'ServiceName', label: 'Service Name', width: 250 },
-    { name: 'ServiceStatus', label: 'Service Status', width: 150 },
-    { name: 'MachineName', label: 'Machine Name', hidden: false, width: 200 },
-    { name: 'DisplayName', label: 'Detailed Name', width: 350 },
-    { name: 'IsWebService', label: 'IsWebService', width: 150 },
-    { name: 'StatusToBeUpdated', label: 'Start Service', width: 150 },
-    { name: 'StatusToBeUpdated1', label: 'Stop Service', width: 150 }
+    { name: 'ServiceName', label: 'Service Name', width: 250,sortable: true },
+    { name: 'ServiceStatus', label: 'Service Status', width: 150,sortable: true },
+    { name: 'MachineName', label: 'Machine Name', hidden: false, width: 200,sortable: true },
+    { name: 'DisplayName', label: 'Detailed Name', width: 350,sortable: true },
+    { name: 'IsWebService', label: 'ServiceType', width: 150,sortable: true },
+    { name: 'StatusToBeUpdated', label: 'Start Service', width: 150,filter:false },
+    { name: 'StatusToBeUpdated1', label: 'Stop Service', width: 150,filter:false }
   ];
   ServiceOption: string = "false";
   data: any[];
+  dataCopy: any[];
   now: Date;
   filteredData: any[];
   filteredTotal: number;
@@ -36,7 +37,7 @@ export class TestDatatableComponent implements OnInit {
   searchTerm: string = '';
   fromRow: number = 1;
   currentPage: number = 1;
-  pageSize: number = 50;
+  pageSize: number = 10;
   sortBy: string = 'ServiceName';
   selectedRows: Services[] = [];
   _response: ServiceResponse[] = [];
@@ -59,6 +60,7 @@ export class TestDatatableComponent implements OnInit {
     this._loadingService.register('stringName');
     this.now = new Date();
     this.data = [];
+    this.dataCopy =[];
     console.log(this.now)
     if (this.ServiceOption == "true" && this.MachineName == '') {
       alert('Server Name is Mandatory');
@@ -66,15 +68,19 @@ export class TestDatatableComponent implements OnInit {
     else if (this.ServiceOption == "true" && this.MachineName != '') {
       this._http.get(CommonConstants.ApiURL + "GetAllServicesByMachineName?MachineName=" + this.MachineName)
         .subscribe(response => {
-          this.data = response.json()
+          this.data = response.json();
+          this.dataCopy = response.json();
           this._loadingService.resolve('stringName');
+          this.filter();
         });
     }
     else {
       this._http.get(CommonConstants.ApiURL + "GetConfiguredServices")
         .subscribe(response => {
           this.data = response.json();
+          this.dataCopy = response.json();
           this._loadingService.resolve('stringName');
+          this.filter();
         });
     }
   }
@@ -105,6 +111,7 @@ export class TestDatatableComponent implements OnInit {
   }
 
   page(pagingEvent: IPageChangeEvent): void {
+    //alert(pagingEvent.fromRow +" - " + pagingEvent.page + " - " + pagingEvent.pageSize)
     this.fromRow = pagingEvent.fromRow;
     this.currentPage = pagingEvent.page;
     this.pageSize = pagingEvent.pageSize;
@@ -117,7 +124,8 @@ export class TestDatatableComponent implements OnInit {
   }
 
   filter(): void {
-    let newData: any[] = this.data;
+    let newData: any[] = this.dataCopy;
+    this.filteredTotal = newData.length;
     let excludedColumns: string[] = this.columns
       .filter((column: ITdDataTableColumn) => {
         return ((column.filter === undefined && column.hidden === true) ||
@@ -125,10 +133,9 @@ export class TestDatatableComponent implements OnInit {
       }).map((column: ITdDataTableColumn) => {
         return column.name;
       });
-    newData = this._dataTableService.filterData(newData, this.searchTerm, true, excludedColumns);
-    this.filteredTotal = newData.length;
-    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
-    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+      newData = this._dataTableService.filterData(newData, this.searchTerm, true, excludedColumns);
+      newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+      newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
     this.data = newData;
   }
 
