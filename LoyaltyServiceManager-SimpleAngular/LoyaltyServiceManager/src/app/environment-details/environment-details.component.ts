d41ad55fc259afdad5b525621ae56a10ad1cd950
Verma <himanshu.verma2@aristocrat.com>
2018-09-07 00:29:20 +0530

    Fixed the searching sorting and paging of environment details page

--- LoyaltyServiceManager-SimpleAngular/LoyaltyServiceManager/src/app/environment-details/environment-details.component.ts
+++ LoyaltyServiceManager-SimpleAngular/LoyaltyServiceManager/src/app/environment-details/environment-details.component.ts
@@ -29,7 +29,7 @@ export class EnvironmentDetailsComponent implements OnInit {
 
   ServiceOption: string = "false";
   data: any[];
-  //apiUrl : string = "http://localhost/Service_Manager_API/api/service";
+  dataCopy: any[];
 
   filteredData: any[];
   filteredTotal: number;
@@ -37,7 +37,7 @@ export class EnvironmentDetailsComponent implements OnInit {
   searchTerm: string = '';
   fromRow: number = 1;
   currentPage: number = 1;
-  pageSize: number = 50;
+  pageSize: number = 10;
   sortBy: string = 'ApplicationName';
   selectedRows: Services[] = [];
   _response: ServiceResponse[]=[];
@@ -46,19 +46,7 @@ export class EnvironmentDetailsComponent implements OnInit {
   sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
   headers : Headers
   options : RequestOptions
-  MachineName: string='';
-
-  //keep the flag true if csv contains header else not, this falg will help in validations.
-  static isHeaderPresentFlag = true; 
- 
-  //keep the flag true if you want to validate each records length to match with header length, false otherwise.
-  static validateHeaderAndRecordLengthFlag = true;
-   
-  //keep the flag true if you want file with only .csv extensions should be read, false otherwise.
-  static valildateFileExtenstionFlag = true;
-  
-  static tokenDelimeter = ",";
-  
+  MachineName: string='';  
   // @ViewChild('fileImportInput')
   fileImportInput: any;
   
@@ -66,8 +54,6 @@ export class EnvironmentDetailsComponent implements OnInit {
    constructor(private _dataTableService: TdDataTableService,private _http: Http, private _httpCLient: HttpClient,private _fileUtil: FileUtil,private _loadingService: TdLoadingService ) {}
 
    ngOnInit(): void {
-    this._defaultResponse.ErrorMessage = "";
-    this._response[0] = this._defaultResponse;
     //this.getEnvironmentDetails();
   }
   getEnvironmentDetails()
@@ -75,20 +61,17 @@ export class EnvironmentDetailsComponent implements OnInit {
     console.log('Get Applications Called')
     this._loadingService.register('stringName1');
     this.data = [];
+    this.dataCopy = [];
       this._http.get(CommonConstants.ApiURL + "GetEnvironmentDetails?ServerName")
       .subscribe(response =>
         {
           this.data=  response.json();
+          this.dataCopy = response.json();
           this.filter();
           this._loadingService.resolve('stringName1');
         });
     
   }
-
-  private extractData(res: Response) {
-    let body = res.json();
-    return body || {};
-}
   sort(sortEvent: ITdDataTableSortChangeEvent): void {
     this.sortBy = sortEvent.name;
     this.sortOrder = sortEvent.order;
@@ -113,68 +96,18 @@ export class EnvironmentDetailsComponent implements OnInit {
   }
 
   filter(): void {
-    let newData: any[] = this.data;
-    let excludedColumns: string[] = this.columns
-    .filter((column: ITdDataTableColumn) => {
-      return ((column.filter === undefined && column.hidden === true) ||
-              (column.filter !== undefined && column.filter === false));
-    }).map((column: ITdDataTableColumn) => {
-      return column.name;
-    });
-    newData = this._dataTableService.filterData(newData, this.searchTerm, true, excludedColumns);
+    let newData: any[] = this.dataCopy;
     this.filteredTotal = newData.length;
-    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
-    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+    let excludedColumns: string[] = this.columns
+      .filter((column: ITdDataTableColumn) => {
+        return ((column.filter === undefined && column.hidden === true) ||
+          (column.filter !== undefined && column.filter === false));
+      }).map((column: ITdDataTableColumn) => {
+        return column.name;
+      });
+      newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+      newData = this._dataTableService.filterData(newData, this.searchTerm, true, excludedColumns);
+      newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
     this.data = newData;
-  }
-
-  // METHOD CALLED WHEN CSV FILE IS IMPORTED
-fileChangeListener($event): void {
-
-  var text = [];
-  var files = $event.srcElement.files;
-
-  if(EnvironmentDetailsComponent.validateHeaderAndRecordLengthFlag){
-    if(!this._fileUtil.isCSVFile(files[0])){
-      alert("Please import valid .csv file.");
-      this.fileReset();
-    }
-  }
-
-  var input = $event.target;
-  var reader = new FileReader();
-  reader.readAsText(input.files[0]);
-  reader.onloadend = function(){
-    alert('Read Ended')
-  }
-  
-  reader.onloadend =  (data: any) => {
-    
-    let csvData = data.target.result;
-    let csvRecordsArray = csvData.split(/\r\n|\n/);
-
-    var headerLength = -1;
-    if(EnvironmentDetailsComponent.isHeaderPresentFlag){
-      let headersRow = this._fileUtil.getHeaderArray(csvRecordsArray, EnvironmentDetailsComponent.tokenDelimeter);
-      headerLength = headersRow.length; 
-    }
-     
-    this.csvRecords = this._fileUtil.getDataRecordsArrayFromCSVFile(csvRecordsArray, 
-        headerLength, EnvironmentDetailsComponent.validateHeaderAndRecordLengthFlag, EnvironmentDetailsComponent.tokenDelimeter);
-     console.log(this.csvRecords)
-    if(this.csvRecords == null){
-      this.fileReset();
-    }
-  }
-
-  reader.onerror = function () {
-    alert('Unable to read ' + input.files[0]);
-  };
-};
-
-fileReset(){
-  // this.fileImportInput.nativeElement.value = "";
-  // this.csvRecords = '';
-}
-  
+  }  
 }
